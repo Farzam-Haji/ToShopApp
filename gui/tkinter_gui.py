b5576df629dfb71060aac0a22d0a2d9c3e651cf7
Farzam Haji <farzam.hajisabili@gmail.com>
2025-05-20 01:00:52 +0330

    introduced and fixed basid functionality of tkraise

--- gui/tkinter_gui.py
+++ gui/tkinter_gui.py
@@ -1,136 +1,106 @@
 import tkinter as tk
-from backend.logic import months
+from backend.logic import fetch_monItems, months
 from backend.database import CommonDB, MonthDB
 from bidi.algorithm import get_display
 import arabic_reshaper
 
-def convert(text):
-	reshaped = arabic_reshaper.reshape(text)
-	return get_display(reshaped)
+
+
+
+
 
 class App(tk.Tk):
 	def __init__(self, mdb:MonthDB, cdb:CommonDB):
 		super().__init__()
+
 		self.title("ToShop App")
 		self.geometry("400x500")
 
-		menubar = tk.Menu(self)
-		self.config(menu=menubar)
-		#
-		menumenu = tk.Menu(menubar, tearoff=0)
-		menubar.add_cascade(label="Menu", menu=menumenu)
-		menumenu.add_command(label="Common")
-		menumenu.add_separator()
-		menumenu.add_command(label="Exit", command=self.destroy)
-		#
-		menuhelp = tk.Menu(menubar, tearoff=0)
-		menubar.add_cascade(label="Help", menu=menuhelp)
-		menuhelp.add_command(label="Help")
+		# Menu
+		menuBar = tk.Menu(self)
+		self.config(menu = menuBar)
 
-		self.frames = {}
-		for F in (MainFrame, MonthFrame):
-			frame = F(self, self, mdb, cdb)
-			self.frames[F.__name__] = frame
-			frame.pack()
+		menuMenu = tk.Menu(menuBar, tearoff=0)
+		menuMenu.add_command(label="Home")
+		menuMenu.add_command(label="Common")
+		menuMenu.add_separator()
+		menuMenu.add_command(label="Exit", command = self.destroy)
+		menuBar.add_cascade(label="Menu", menu=menuMenu)
 
-		self.show_frame("MainFrame")
+		helpMenu = tk.Menu(menuBar, tearoff=0)
+		helpMenu.add_command(label="Help")
+		menuBar.add_cascade(label="Help", menu=helpMenu)
+
+		mainFrame = tk.Frame(self)
+		mainFrame.pack(fill="both", expand=True)
+
+		self.frames = {"main":MainFrame(self, mainFrame, mdb), "month":MonthFrame(self, mainFrame, mdb)}
+		self.show_frame("main")
+
+	
 
 	def show_frame(self, name):
+		if name == "month":
+			self.frames["main"].forget()
+		else:
+			self.frames["month"].forget()
+
 		frame = self.frames[name]
 		frame.tkraise()
+		frame.pack(fill='both', expand=True)
+
+	def convert(self, text):
+		reshaped = arabic_reshaper.reshape(text)
+		return get_display(reshaped)
 
 
 class MainFrame(tk.Frame):
-	def __init__(self, parent, controller, mdb, cdb):
-		super().__init__(parent)
+	def __init__(self, parrent, mainFrame, mdb):
+		super().__init__(mainFrame)
 
-		topframe = tk.Frame()
-		topframe.pack(fill='x')
+		# Upper Frame
+		topFrame = tk.Frame(self)
 
-		topframe.columnconfigure(0, weight=9)
-		topframe.columnconfigure(1, weight=1)
+		topFrame.columnconfigure(0, weight=9)
+		topFrame.columnconfigure(1, weight=1)
 
-		monLabel = tk.Label(topframe, text="Months", font=("Arial", 20)).grid(row=0, column=0, rowspan=2)
-		comButton = tk.Button(topframe, text="Common List").grid(row=0, column=1, sticky=tk.W+tk.E)
-		helpButton = tk.Button(topframe, text="Help").grid(row=1, column=1, sticky=tk.W+tk.E)
+		tk.Label(topFrame, text="Months", font=("Arial", 20)).grid(row=0, column=0, rowspan=2)
+		tk.Button(topFrame, text="Common List").grid(row=0, column=1, sticky=tk.W+tk.E)
+		tk.Button(topFrame, text="Help").grid(row=1, column=1, sticky=tk.W+tk.E)
 
+		topFrame.pack(fill='x')
 
-		monthFrame = tk.Frame()
-		monthFrame.pack(fill='both', expand=True)
+		# Bottom Frame
+		botFrame = tk.Frame(self)
 
 		for i in range(3):
-			monthFrame.columnconfigure(i, weight=1)
+			botFrame.columnconfigure(i, weight=1)
+
 		for i in range(4):
-			monthFrame.rowconfigure(i, weight=1)
+			botFrame.rowconfigure(i, weight=1)
 
 		monlist = months(mdb)
 		for i, month in enumerate(monlist):
-			row = i // 3 
-			col = i % 3
-			but = tk.Button(monthFrame, text=convert(month), font=("Arial", 14))
-			but.grid(row=row, column=col, sticky="nsew")
+			but = tk.Button(botFrame, text=parrent.convert(month), font=("Arial", 14), command = lambda:parrent.show_frame("month"))
+			but.grid(row=i // 3, column=i % 3, sticky="nsew")
 
+		botFrame.pack(fill='both', expand=True)
 
-
-
-
-
+		self.pack(fill='both', expand=True)
 
 class MonthFrame(tk.Frame):
-	def __init__(self, parent, controller, mdb, cdb):
-		super().__init__(parent)
+	def __init__(self, parrent, mainFrame, mdb):
+		super().__init__(mainFrame)
 
-class CommonWindow(tk.Toplevel):
-	pass
+		items, cost = fetch_monItems(mdb,2)
+		for i, item in enumerate(items):
+			tk.Label(self, text=f"{i+1}. {item}").pack()
 
-
-
-
-
-# def start_gui(mdb:MonthDB):
-# 	root = tk.Tk()
-# 	root.geometry("400x500")
-# 	root.title("To Shop App")
-
-# 	menubar = tk.Menu(root)
-# 	#
-# 	menumenu = tk.Menu(menubar, tearoff=0)
-# 	menubar.add_cascade(label="Menu", menu=menumenu)
-# 	menumenu.add_command(label="Common")
-# 	menumenu.add_separator()
-# 	menumenu.add_command(label="Exit", command=root.destroy)
-# 	#
-# 	menuhelp = tk.Menu(menubar, tearoff=0)
-# 	menubar.add_cascade(label="Help", menu=menuhelp)
-# 	menuhelp.add_command(label="Help")
+		tk.Label(self, text=f"cost= {str(cost)}").pack()
+		# monframe = tk.Frame(self)
+		# tk.Label(monframe, text="month items").pack()
+		# monframe.pack()
 
 
-# 	topframe = tk.Frame()
-# 	topframe.pack(fill='x')
+		self.pack(fill='both', expand=True)
 
-# 	topframe.columnconfigure(0, weight=9)
-# 	topframe.columnconfigure(1, weight=1)
-
-# 	monLabel = tk.Label(topframe, text="Months", font=("Arial", 20)).grid(row=0, column=0, rowspan=2)
-# 	comButton = tk.Button(topframe, text="Common List").grid(row=0, column=1, sticky=tk.W+tk.E)
-# 	helpButton = tk.Button(topframe, text="Help").grid(row=1, column=1, sticky=tk.W+tk.E)
-
-
-# 	monthFrame = tk.Frame(root)
-# 	monthFrame.pack(fill='both', expand=True)
-
-# 	for i in range(3):
-# 		monthFrame.columnconfigure(i, weight=1)
-# 	for i in range(4):
-# 		monthFrame.rowconfigure(i, weight=1)
-
-# 	monlist = months(mdb)
-# 	for i, month in enumerate(monlist):
-# 		row = i // 3 
-# 		col = i % 3
-# 		but = tk.Button(monthFrame, text=convert(month), font=("Arial", 14))
-# 		but.grid(row=row, column=col, sticky="nsew")
-
-
-# 	root.config(menu = menubar)
-# 	root.mainloop()
