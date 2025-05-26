cba6187baf2a209170236df905076aa063cd3210
Farzam Haji <farzam.hajisabili@gmail.com>
2025-05-26 22:05:16 +0330

    different month can be opened and month items get refresh according to each month

--- gui/tkinter_gui.py
+++ gui/tkinter_gui.py
@@ -1,9 +1,7 @@
 
-
-from enum import verify
 import tkinter as tk
-from tkinter import BOTH, StringVar, YView, ttk
-from backend.logic import fetch_comItems, fetch_monItems, months
+from tkinter import ttk
+from backend.logic import add_monCost, del_monItem, fetch_comItems, fetch_monItems, months
 from backend.database import CommonDB, MonthDB
 from bidi.algorithm import get_display
 import arabic_reshaper
@@ -23,17 +21,58 @@ from PIL import Image, ImageTk
 global MonthInteger
 MonthInteger = 1
 
+def handle_delete(mdb, entry, textbox):
+    itemindex = int(entry.get())
+    mitems = fetch_monItems(mdb, MonthInteger)[0]
+    if 1 <= itemindex <= len(mitems):
+        item = mitems[itemindex - 1]
+        del_monItem(mdb, MonthInteger, item)
+        entry.delete(0, tk.END)
+        refresh_month(textbox, mdb)
+    else:
+        entry.delete(0, tk.END)
+        print("invalid index")
 
-def switch_month(homFrame, monFrame, *monthInt):
-	# global MonthInteger
+
+# def find_month_item(mdb, itemindex):
+# 	ind = int(itemindex)-1
+# 	mitems = fetch_monItems(mdb, MonthInteger)[0]
+# 	if ind > len(mitems) or ind < 0:
+# 		return error
+# 	else:
+# 		return mitems[ind]
+
+
+def refresh_common(text:tk.Text, cdb):
+	text.delete('1.0', tk.END)
+	text.insert(tk.END, "Month Items:\n\n")
+
+	citems, ccost = fetch_comItems(cdb)
+	for i, item in enumerate(citems):
+		text.insert(tk.END, f"{i+1}. {item}\n")
+
+	text.insert(tk.END, f"\n\n\n*Estimated Cost: {ccost}*")
+
+def refresh_month(text:tk.Text, mdb):
+	text.delete('1.0', tk.END)
+	text.insert(tk.END, "Month Items:\n\n")
+
+	mitems, mcost = fetch_monItems(mdb, MonthInteger)
+	for i, item in enumerate(mitems):
+		text.insert(tk.END, f"{i+1}. {item}\n")
+
+	text.insert(tk.END, f"\n\n\n*Estimated Cost: {mcost}*")
+
+def switch_month(homFrame, monFrame, mdb, text:tk.Text, *monthInt):
+	global MonthInteger
 	if monthInt:
 		MonthInteger = monthInt[0]
-		print (monthInt)
-		print (monthInt[0])
-		print (MonthInteger)
 		
 	homFrame.forget()
 	monFrame.tkraise()
+
+	# Adding the items in textbox
+	refresh_month(text, mdb)
 	monFrame.pack(fill='both', expand=True)
 
 
@@ -124,11 +163,60 @@ def start_gui(mdb:MonthDB, cdb:CommonDB):
 	paperimg = Image.open("assets/2.png")
 	papertk = ImageTk.PhotoImage(paperimg)
 
-
-
 	# Main frame config
 	frame = tk.Frame(app)
 	frame.pack(fill="both", expand=True)
+
+
+
+	# Month frame
+	monthFrame = tk.Frame(frame)
+	# Grid configuration
+	for i in range (5):
+		monthFrame.columnconfigure(i, weight=1)
+		monthFrame.rowconfigure(i, weight=1)
+	monthFrame.rowconfigure(0, weight=2)
+
+	# Text box for items	
+	textFrame = tk.Frame(monthFrame)
+	textFrame.grid(row=0, column=0, rowspan=2, columnspan=5, sticky='nsew')
+
+	textbox = tk.Text(textFrame, relief='groove', bd=0, bg='#f5f5f5')
+	textbox.pack(fill='both', expand=True)
+
+	verticalScroll = tk.Scrollbar(textbox, command=textbox.yview)
+	verticalScroll.pack(side=tk.RIGHT, fill=tk.Y)
+	textbox.configure(yscrollcommand=verticalScroll.set)
+
+	textbox.insert(tk.END, "Month Items:\n\n")
+
+	# Buttons and entries for controlling the item list
+	ttk.Separator(monthFrame, orient=tk.VERTICAL).grid(column=2, row=2, sticky='ns')
+	ttk.Separator(monthFrame, orient=tk.HORIZONTAL).grid(column=0, row=3, columnspan=5, sticky='ew')
+	ttk.Separator(monthFrame, orient=tk.VERTICAL).grid(column=3, row=4, sticky='ns')
+
+	addcostBut = tk.Button(monthFrame, text="Add Cost", font=("",9), command=lambda: (
+		add_monCost(mdb, MonthInteger, addcostEntry.get()),
+		addcostEntry.delete(0, tk.END),
+		refresh_month(textbox, mdb)
+		)
+	)
+	addcostBut.grid(row=2, column=0, sticky='ew')
+	addcostEntry = tk.Entry(monthFrame, width=10)
+	addcostEntry.grid(row=2, column=1, sticky='ew')
+
+	delItemBut = tk.Button(monthFrame, text="Remove", font=("",9), command=lambda: handle_delete(mdb, delItemEntry, textbox))
+	delItemBut.grid(row=2, column=3, sticky='ew')
+	delItemEntry = tk.Entry(monthFrame, width=10)
+	delItemEntry.grid(row=2, column=4, sticky='ew')
+
+	addItemBut = tk.Button(monthFrame, text="Add Item", font=("",9))
+	addItemBut.grid(row=4, column=0, sticky='ew')
+	addItemEntry = tk.Entry(monthFrame)
+	addItemEntry.grid(row=4, column=1, columnspan=2, sticky='ew')
+
+	addCommonBut = tk.Button(monthFrame, text="Add Common", font=("", 9))
+	addCommonBut.grid(row=4, column=4, sticky='ew')
 
 
 
@@ -158,62 +246,11 @@ def start_gui(mdb:MonthDB, cdb:CommonDB):
 
 	monlist = months(mdb)
 	for i, month in enumerate(monlist):
-		but = tk.Button(botFrame, text=convert(month), font=("Arial", 14), command=lambda m=i+1: switch_month(homeFrame, monthFrame, m))
+		but = tk.Button(botFrame, text=convert(month), font=("Arial", 14), command=lambda m=i+1: switch_month(homeFrame, monthFrame, mdb, textbox, m))
 		but.grid(row=i // 3, column=i % 3, sticky="nsew")
 
 	botFrame.pack(fill='both', expand=True)
 
-	
-
-	# Month frame
-	monthFrame = tk.Frame(frame)
-	# Grid configuration
-	for i in range (5):
-		monthFrame.columnconfigure(i, weight=1)
-		monthFrame.rowconfigure(i, weight=1)
-	monthFrame.rowconfigure(0, weight=2)
-
-	# Text box for items	
-	textFrame = tk.Frame(monthFrame)
-	textFrame.grid(row=0, column=0, rowspan=2, columnspan=5, sticky='nsew')
-
-	textbox = tk.Text(textFrame, relief='groove', bd=0, bg='#f5f5f5')
-	textbox.pack(fill='both', expand=True)
-
-	verticalScroll = tk.Scrollbar(textbox, command=textbox.yview)
-	verticalScroll.pack(side=tk.RIGHT, fill=tk.Y)
-	textbox.configure(yscrollcommand=verticalScroll.set)
-
-	textbox.insert(tk.END, "Month Items:\n\n")
-
-	mitems, mcost = fetch_monItems(mdb, MonthInteger)
-	for i, item in enumerate(mitems):
-		textbox.insert(tk.END, f"{i+1}. {item}\n")
-
-	textbox.insert(tk.END, f"\n\n\n*Estimated Cost: {mcost}*")
-
-	# Buttons and entries for controlling the item list
-	ttk.Separator(monthFrame, orient=tk.VERTICAL).grid(column=2, row=2, sticky='ns')
-	ttk.Separator(monthFrame, orient=tk.HORIZONTAL).grid(column=0, row=3, columnspan=5, sticky='ew')
-	ttk.Separator(monthFrame, orient=tk.VERTICAL).grid(column=3, row=4, sticky='ns')
-
-	addcostBut = tk.Button(monthFrame, text="Add Cost", font=("",9))
-	addcostBut.grid(row=2, column=0, sticky='ew')
-	addcostEntry = tk.Entry(monthFrame, width=10)
-	addcostEntry.grid(row=2, column=1, sticky='ew')
-
-	delItemBut = tk.Button(monthFrame, text="Remove", font=("",9))
-	delItemBut.grid(row=2, column=3, sticky='ew')
-	delItemEntry = tk.Entry(monthFrame, width=10)
-	delItemEntry.grid(row=2, column=4, sticky='ew')
-
-	addItemBut = tk.Button(monthFrame, text="Add Item", font=("",9))
-	addItemBut.grid(row=4, column=0, sticky='ew')
-	addItemEntry = tk.Entry(monthFrame)
-	addItemEntry.grid(row=4, column=1, columnspan=2, sticky='ew')
-
-	addCommonBut = tk.Button(monthFrame, text="Add Common", font=("", 9))
-	addCommonBut.grid(row=4, column=4, sticky='ew')
 
 
 
